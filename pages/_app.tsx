35c474e75987714836199883c69e742b2e220eed
Embrace <we@embrace.community>
2022-10-26 12:40:45 +0100

    wallet-connect buggy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { chains, providers } from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+
+const NEXT_PUBLIC_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+const modalConfig = {
+  projectId: NEXT_PUBLIC_PROJECT_ID,
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: "web3Modal",
+    autoConnect: true,
+    chains: [chains.mainnet, chains.goerli],
+    providers: [
+      // providers.walletConnectProvider({ projectId: NEXT_PUBLIC_PROJECT_ID }),
+      // providers.jsonRpcProvider({
+      //   rpc: (chain) => ({
+      //     http: `https://evm.cronos.org/`,
+      //   }),
+      // }),
+      providers.alchemyProvider({ apiKey: "0yIqvW3tQ-mvKDs59KTADfAffOkAFFP4" }),
+    ],
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Web3Modal config={modalConfig} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { ConnectButton, useAccount, AccountButton } from "@web3modal/react";
+import ClientOnlyWrapper from "../components/ClientOnlyWrapper";
 
 const Home: NextPage = () => {
+  const { isConnected, address } = useAccount();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +16,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Embrace!</main>
+      <main className={styles.main}>
+        <ClientOnlyWrapper>
+          Embrace!
+          {isConnected ? (
+            <>
+              <h1>{address ? address : "none"} </h1>
+            </>
+          ) : (
+            <ConnectButton />
+          )}
+        </ClientOnlyWrapper>
+      </main>
 
       <footer className={styles.footer}>.</footer>
     </div>
